28c146bc60b384ae0ce8e99b5d6a050d55f0bee4
Venilton Junior <venilton.junior.dev@gmail.com>
2025-08-06 19:35:51 -0300

    feat: remove useless method and add deactivate

--- src/domain/protocols/repositories/IPasswordCommandRepository.ts
+++ src/domain/protocols/repositories/IPasswordCommandRepository.ts
@@ -3,6 +3,5 @@ import { Id } from '@/domain/valueObjects'
 
 export interface IPasswordCommandRepository {
   create: (entity: Password) => Promise<Password>
-  softDelete: (id: Id) => Promise<void>
-  hardDelete: (id: Id) => Promise<void>
+  deactivate: (id: Id) => Promise<void>
 }

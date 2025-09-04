79ca3be8dbf0f491c0bb652658876b38c462b51f
Venilton Junior <venilton.junior.dev@gmail.com>
2025-09-03 21:55:38 -0300

    fix: add await

--- src/domain/usecases/password/ResetPassword.ts
+++ src/domain/usecases/password/ResetPassword.ts
@@ -3,8 +3,8 @@ import { IResetPassword, IPasswordCommandRepository } from "@/domain/protocols";
 
 export class ResetPassword implements IResetPassword {
   constructor(private readonly repository: IPasswordCommandRepository) {}
-  execute(new_password: Password, old_password: Password): Promise<Password> {
-    this.repository.deactivate(old_password.getId())
-    return this.repository.create(new_password)
+  async execute(new_password: Password, old_password: Password): Promise<Password> {
+    await this.repository.deactivate(old_password.getId())
+    return await this.repository.create(new_password)
   }
 }

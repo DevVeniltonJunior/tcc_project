c2168460ab27d491dedf66afaabac876a3f641c8
Venilton Junior <venilton.junior.dev@gmail.com>
2025-09-17 20:41:36 -0300

    chore: setup swagger

--- src/infra/config/server/App.ts
+++ src/infra/config/server/App.ts
@@ -2,11 +2,16 @@ import 'module-alias/register'
 import express from 'express'
 import { router } from './Router'
 import 'dotenv/config'
+import { setupSwagger } from "../../../../Swagger"
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/', router)
+setupSwagger(app)
 
-app.listen(3000, () => console.log('Server is running in http://localhost:3000'))
+app.listen(3000, () => {
+  console.log('🚀 Server is running in http://localhost:3000')
+  console.log("📖 Docs: http://localhost:3000/api-docs")
+})

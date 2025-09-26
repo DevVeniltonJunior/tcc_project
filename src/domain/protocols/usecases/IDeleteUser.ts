72e95026c02d170d020ece005adaca2416caac8b
Venilton Junior <venilton.junior.dev@gmail.com>
2025-09-26 20:21:29 -0300

    feat: add option to delete permanet

--- src/domain/protocols/usecases/IDeleteUser.ts
+++ src/domain/protocols/usecases/IDeleteUser.ts
@@ -1,5 +1,5 @@
 import { Id } from '@/domain/valueObjects'
 
 export interface IDeleteUser {
-  execute: (id: Id) => Promise<void>
+  execute: (id: Id, isPermanent: boolean) => Promise<void>
 }
--- src/domain/usecases/user/DeleteUser.ts
+++ src/domain/usecases/user/DeleteUser.ts
@@ -4,7 +4,12 @@ import { Id } from '@/domain/valueObjects'
 export class DeleteUser implements IDeleteUser {
   constructor(private readonly repository: IUserCommandRepository) {}
 
-  public async execute(id: Id): Promise<void> {
+  public async execute(id: Id, isPermanent: boolean): Promise<void> {
+    if (isPermanent) {
+      await this.repository.hardDelete(id)
+      return
+    }
+    
     await this.repository.softDelete(id)
   }
 }
